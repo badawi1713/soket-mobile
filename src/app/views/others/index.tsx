58fd89772af841c4bc5ff904ffaa41e593931ca0
Dzaky <dzaky.badawi@gmail.com>
2025-01-06 17:25:57 +0700

    fix(others): change app url and name

--- src/app/views/others/index.tsx
+++ src/app/views/others/index.tsx
@@ -1,7 +1,20 @@
 import Typography from '@/components/Typography';
 import images from '@/constants/images';
 import React from 'react';
-import { Image, ScrollView, TouchableOpacity, View } from 'react-native';
+import { Image, Linking, ScrollView, TouchableOpacity, View } from 'react-native';
+import { toast } from 'sonner-native';
+
+const openExternalLink = async (url: string) => {
+	await Linking.canOpenURL(url)
+		.then((supported) => {
+			if (supported) {
+				Linking.openURL(url);
+			} else {
+				toast.error(`Don't know how to open URI: ${url}`);
+			}
+		})
+		.catch((err) => toast.error(`An error occurred ${err}`));
+};
 
 const Content = () => {
 	return (
@@ -10,40 +23,13 @@ const Content = () => {
 				Featured Apps
 			</Typography>
 			<ScrollView contentContainerStyle={{ flexGrow: 1, gap: 16, paddingHorizontal: 16, paddingVertical: 16 }}>
-				<TouchableOpacity className="flex-row items-center gap-4 py-3 border-b border-b-border-light">
-					<Image className="w-12 h-12" resizeMode="contain" source={images.imgApp1} />
+				<TouchableOpacity
+					className="flex-row items-center gap-4 py-3 border-b border-b-border-light"
+					onPress={() => openExternalLink('http://10.7.1.116')}
+				>
+					<Image className="w-12 h-12" resizeMode="contain" source={images.imgLogoApp} />
 					<Typography variant="body1" weight="semibold">
-						PKU
-					</Typography>
-				</TouchableOpacity>
-				<TouchableOpacity className="flex-row items-center gap-4 py-3 border-b border-b-border-light">
-					<Image className="w-12 h-12" resizeMode="contain" source={images.imgApp1} />
-					<Typography variant="body1" weight="semibold">
-						PKU
-					</Typography>
-				</TouchableOpacity>
-				<TouchableOpacity className="flex-row items-center gap-4 py-3 border-b border-b-border-light">
-					<Image className="w-12 h-12" resizeMode="contain" source={images.imgApp1} />
-					<Typography variant="body1" weight="semibold">
-						PKU
-					</Typography>
-				</TouchableOpacity>
-				<TouchableOpacity className="flex-row items-center gap-4 py-3 border-b border-b-border-light">
-					<Image className="w-12 h-12" resizeMode="contain" source={images.imgApp1} />
-					<Typography variant="body1" weight="semibold">
-						PKU
-					</Typography>
-				</TouchableOpacity>
-				<TouchableOpacity className="flex-row items-center gap-4 py-3 border-b border-b-border-light">
-					<Image className="w-12 h-12" resizeMode="contain" source={images.imgApp1} />
-					<Typography variant="body1" weight="semibold">
-						PKU
-					</Typography>
-				</TouchableOpacity>
-				<TouchableOpacity className="flex-row items-center gap-4 py-3 border-b border-b-border-light">
-					<Image className="w-12 h-12" resizeMode="contain" source={images.imgApp1} />
-					<Typography variant="body1" weight="semibold">
-						PKU
+						SOKET Tools
 					</Typography>
 				</TouchableOpacity>
 			</ScrollView>
--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,9 +1,11 @@
 import imgApp1 from '@/assets/images/img-app-1.png';
 import imgLogin from '@/assets/images/img-login-2.png';
 import imgPlantExample from '@/assets/images/img-plant-example.jpg';
+import imgLogoApp from '@/assets/images/logo-app.png';
 
 export default {
 	imgLogin,
 	imgPlantExample,
 	imgApp1,
+	imgLogoApp,
 };
