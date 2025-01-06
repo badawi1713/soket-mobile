ecfc6f125ee9d87a866ef9116eee872a8deab6e2
Dzaky <dzaky.badawi@gmail.com>
2025-01-06 18:24:04 +0700

    feat(others): add in app browser functionality

--- src/app/views/others/index.tsx
+++ src/app/views/others/index.tsx
@@ -1,19 +1,34 @@
 import Typography from '@/components/Typography';
+import { COLORS } from '@/constants/colors';
 import images from '@/constants/images';
 import React from 'react';
 import { Image, Linking, ScrollView, TouchableOpacity, View } from 'react-native';
+import InAppBrowser from 'react-native-inappbrowser-reborn';
 import { toast } from 'sonner-native';
 
-const openExternalLink = async (url: string) => {
-	await Linking.canOpenURL(url)
-		.then((supported) => {
-			if (supported) {
-				Linking.openURL(url);
-			} else {
-				toast.error(`Don't know how to open URI: ${url}`);
-			}
-		})
-		.catch((err) => toast.error(`An error occurred ${err}`));
+const openInAppBrowser = async (url: string) => {
+	try {
+		const isAvailable = await InAppBrowser.isAvailable();
+		if (isAvailable) {
+			await InAppBrowser.open(url, {
+				dismissButtonStyle: 'close',
+				preferredBarTintColor: COLORS.primary.main,
+				preferredControlTintColor: 'white',
+				readerMode: false,
+				animated: true,
+				modalPresentationStyle: 'fullScreen',
+				modalTransitionStyle: 'coverVertical',
+				modalEnabled: true,
+				enableBarCollapsing: false,
+			});
+		} else {
+			toast.error('InAppBrowser is not available in this device. Open with default browser.');
+			Linking.openURL(url);
+		}
+	} catch (_) {
+		toast.error('InAppBrowser is not available in this device. Open with default browser.');
+		Linking.openURL(url);
+	}
 };
 
 const Content = () => {
@@ -25,7 +40,7 @@ const Content = () => {
 			<ScrollView contentContainerStyle={{ flexGrow: 1, gap: 16, paddingHorizontal: 16, paddingVertical: 16 }}>
 				<TouchableOpacity
 					className="flex-row items-center gap-4 py-3 border-b border-b-border-light"
-					onPress={() => openExternalLink('http://10.7.1.116')}
+					onPress={() => openInAppBrowser('http://10.7.1.116')}
 				>
 					<Image className="w-12 h-12" resizeMode="contain" source={images.imgLogoApp} />
 					<Typography variant="body1" weight="semibold">
