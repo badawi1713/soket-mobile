d0ee82bf1ac4b949dfe853138331356ab21c60d9
Dzaky <dzaky.badawi@gmail.com>
2025-01-07 14:02:12 +0700

    fix(reliability): hide failure prediction case management

--- src/app/views/reliability/index.tsx
+++ src/app/views/reliability/index.tsx
@@ -155,113 +155,115 @@ const Content = () => {
 						/>
 					</View>
 				</View>
-				<View className="flex-col gap-4 bg-background-paper p-4 rounded-lg">
-					<Typography weight="semibold">Failure Prediction</Typography>
-					<View className="flex-row justify-between gap-x-4">
-						<Card
-							onPress={() =>
-								navigation.navigate('case-details', {
-									title: 'Failure Prediction',
-									subtitle: 'PLTU Tanjung Awar-Awar - Unit 1',
-									type: 'open',
-								})
-							}
-							title="0"
-							variant="error"
-							subtitle="NEW"
-							style={{
-								flex: 1,
-								borderWidth: 1,
-								borderColor: COLORS.border.light,
-								alignItems: 'center',
-							}}
-						/>
-						<Card
-							onPress={() =>
-								navigation.navigate('case-details', {
-									title: 'Failure Prediction',
-									subtitle: 'PLTU Tanjung Awar-Awar - Unit 1',
-									type: 'open',
-								})
-							}
-							title="20"
-							variant="info"
-							subtitle="OPEN"
-							style={{
-								flex: 1,
-								borderWidth: 1,
-								borderColor: COLORS.border.light,
-								alignItems: 'center',
-							}}
-						/>
+				{false && (
+					<View className="flex-col gap-4 bg-background-paper p-4 rounded-lg">
+						<Typography weight="semibold">Failure Prediction</Typography>
+						<View className="flex-row justify-between gap-x-4">
+							<Card
+								onPress={() =>
+									navigation.navigate('case-details', {
+										title: 'Failure Prediction',
+										subtitle: 'PLTU Tanjung Awar-Awar - Unit 1',
+										type: 'open',
+									})
+								}
+								title="0"
+								variant="error"
+								subtitle="NEW"
+								style={{
+									flex: 1,
+									borderWidth: 1,
+									borderColor: COLORS.border.light,
+									alignItems: 'center',
+								}}
+							/>
+							<Card
+								onPress={() =>
+									navigation.navigate('case-details', {
+										title: 'Failure Prediction',
+										subtitle: 'PLTU Tanjung Awar-Awar - Unit 1',
+										type: 'open',
+									})
+								}
+								title="20"
+								variant="info"
+								subtitle="OPEN"
+								style={{
+									flex: 1,
+									borderWidth: 1,
+									borderColor: COLORS.border.light,
+									alignItems: 'center',
+								}}
+							/>
+						</View>
+						<View className="flex-row justify-between flex-wrap gap-x-0">
+							<Card
+								onPress={() =>
+									navigation.navigate('case-details', {
+										title: 'Failure Prediction',
+										subtitle: 'PLTU Tanjung Awar-Awar - Unit 1',
+										type: 'awaiting',
+									})
+								}
+								title="30"
+								variant="default"
+								subtitle="AWAITING"
+								style={{
+									alignItems: 'center',
+									padding: 0,
+								}}
+							/>
+							<Card
+								onPress={() =>
+									navigation.navigate('case-details', {
+										title: 'Failure Prediction',
+										subtitle: 'PLTU Tanjung Awar-Awar - Unit 1',
+										type: 'in-progress',
+									})
+								}
+								title="50"
+								variant="default"
+								subtitle="IN PROGRESS"
+								style={{
+									alignItems: 'center',
+									padding: 0,
+								}}
+							/>
+							<Card
+								onPress={() =>
+									navigation.navigate('case-details', {
+										title: 'Failure Prediction',
+										subtitle: 'PLTU Tanjung Awar-Awar - Unit 1',
+										type: 'completed',
+									})
+								}
+								title="80"
+								variant="default"
+								subtitle="COMPLETED"
+								style={{
+									alignItems: 'center',
+									padding: 0,
+								}}
+							/>
+							<Card
+								onPress={() =>
+									navigation.navigate('case-details', {
+										title: 'Failure Prediction',
+										subtitle: 'PLTU Tanjung Awar-Awar - Unit 1',
+										type: 'closed',
+									})
+								}
+								title="120"
+								variant="default"
+								subtitle="CLOSED"
+								style={{
+									alignItems: 'center',
+									padding: 0,
+								}}
+							/>
+						</View>
 					</View>
-					<View className="flex-row justify-between flex-wrap gap-x-0">
-						<Card
-							onPress={() =>
-								navigation.navigate('case-details', {
-									title: 'Failure Prediction',
-									subtitle: 'PLTU Tanjung Awar-Awar - Unit 1',
-									type: 'awaiting',
-								})
-							}
-							title="30"
-							variant="default"
-							subtitle="AWAITING"
-							style={{
-								alignItems: 'center',
-								padding: 0,
-							}}
-						/>
-						<Card
-							onPress={() =>
-								navigation.navigate('case-details', {
-									title: 'Failure Prediction',
-									subtitle: 'PLTU Tanjung Awar-Awar - Unit 1',
-									type: 'in-progress',
-								})
-							}
-							title="50"
-							variant="default"
-							subtitle="IN PROGRESS"
-							style={{
-								alignItems: 'center',
-								padding: 0,
-							}}
-						/>
-						<Card
-							onPress={() =>
-								navigation.navigate('case-details', {
-									title: 'Failure Prediction',
-									subtitle: 'PLTU Tanjung Awar-Awar - Unit 1',
-									type: 'completed',
-								})
-							}
-							title="80"
-							variant="default"
-							subtitle="COMPLETED"
-							style={{
-								alignItems: 'center',
-								padding: 0,
-							}}
-						/>
-						<Card
-							onPress={() =>
-								navigation.navigate('case-details', {
-									title: 'Failure Prediction',
-									subtitle: 'PLTU Tanjung Awar-Awar - Unit 1',
-									type: 'closed',
-								})
-							}
-							title="120"
-							variant="default"
-							subtitle="CLOSED"
-							style={{
-								alignItems: 'center',
-								padding: 0,
-							}}
-						/>
-					</View>
-				</View>
+				)}
 			</ScrollView>
 		</View>
 	);
